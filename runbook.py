c7516d6ad1271effe121273bbfd6f79386f3cf5f
nick <nickrus@cisco.com>
2019-12-19 22:26:41 +0000

    new format mods, untested

--- runbook.py
+++ runbook.py
@@ -10,7 +10,6 @@ from nornir import InitNornir
 from nornir.plugins.tasks.networking import netmiko_send_command
 from nornir.plugins.tasks.data import load_yaml
 import xmltodict
-# import json
 
 
 def run_checks(task):
@@ -21,7 +20,6 @@ def run_checks(task):
     """
     checks = task.run(task=load_yaml, file=f"host_vars/{task.host.name}.yaml")
 
-    cmds = []
     for chk in checks[0].result["checks"]:
         cmd = (
             "packet-tracer input "
@@ -30,12 +28,7 @@ def run_checks(task):
             f"{chk['dst_ip']} {chk['dst_port']} "
             "xml"
         )
-        cmds.append(cmd)
-
-    for cmd in cmds:
         task.run(task=netmiko_send_command, command_string=cmd)
-
-    return cmds
 
 
 def main():
@@ -50,24 +43,22 @@ def main():
     result = nornir.run(task=run_checks)
     for host, mresult in result.items():
         print(f"Results for {host}")
-        breakpoint()
         # 0 is return value from run_checks
         # 1 is result from load_yaml
         # 2+ are the netmiko command results
-        for cmd, output in zip(mresult[0].result, mresult[2:]):
-            xml_text = f"<root>{output.result}</root>"
-            data = xmltodict.parse(xml_text)
-            # print(json.dumps(data, indent=2))
-
-            print(f"\n- {cmd}")
-            failed = False
-            for phase in data["root"]["Phase"]:
-                print(f"  {phase['id']}/{phase['type']} -> ", end="")
-                if phase["result"] == "DROP":
-                    print(f"DROP: {data['root']['result']['drop-reason']}")
-                    failed = True
-            if not failed:
-                print("ALLOW")
+        checks = mresult[1].result["checks"]
+        for chk, output in zip(checks, mresult[2:]):
+            data = xmltodict.parse(f"<root>{output.result}</root>")
+            # import json; print(json.dumps(data, indent=2))
+            
+            src_intf = f"{data['root']['result']['input-interface']}"
+            dst_intf = f"{data['root']['result']['output-interface']}"
+            print(
+                f"{chk['proto']} "
+                f"{src_intf}/{chk['src_ip']}/{chk['src_port']}->"
+                f"{dst_intf}/{chk['dst_ip']}/{chk['dst_port']}: "
+                f"{data['root']['result']['action']}"
+            )
 
 
 if __name__ == "__main__":
